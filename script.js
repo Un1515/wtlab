e480dbf67e3ca40ff50e0a7e8e6c5acf1104b79f
Un1515 <117341760+Un1515@users.noreply.github.com>
2022-11-27 16:25:07 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".typing" , {
+    strings: ["an IT Student","a YouTuber","a Front-End Web Developer","a Designer"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+});
+var typed1 = new Typed(".typing1" , {
+    strings: ["an IT Student","a YouTuber","a Front-End Web Developer","a Designer"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+});
